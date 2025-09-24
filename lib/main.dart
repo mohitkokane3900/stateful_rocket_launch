7b911f794d7d1b8d1360b6a4c664cc9c102299ff
Mohit Kokane <mohitkokane12@gmail.com>
2025-09-24 18:27:20 -0400

    Initialize Flutter app with counter state and simple display

--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,40 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
       title: 'Rocket Launch Controller',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      // A widget that will be started on the application startup
-      home: CounterWidget(),
+      theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.indigo),
+      home: const LaunchController(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class CounterWidget extends StatefulWidget {
+class LaunchController extends StatefulWidget {
+  const LaunchController({super.key});
+
   @override
-  _CounterWidgetState createState() => _CounterWidgetState();
+  State<LaunchController> createState() => _LaunchControllerState();
 }
 
-class _CounterWidgetState extends State<CounterWidget> {
-  //set counter value
+class _LaunchControllerState extends State<LaunchController> {
   int _counter = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Rocket Launch Controller')),
-      //set up the widget alignement
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: Container(
-              color: Colors.blue,
-              child: Text(
-                //to displays current number
-                '$_counter',
-                style: TextStyle(fontSize: 50.0),
-              ),
-            ),
-          ),
-          Slider(
-            min: 0,
-            max: 100,
-            value: _counter.toDouble(),
-            onChanged: (double value) {
-              setState(() {
-                _counter = value.toInt();
-              });
-            },
-            activeColor: Colors.blue,
-            inactiveColor: Colors.red,
-          ),
-        ],
+      body: Center(
+        child: Text(
+          '$_counter',
+          style: const TextStyle(fontSize: 64, fontWeight: FontWeight.w700),
+        ),
       ),
     );
   }
